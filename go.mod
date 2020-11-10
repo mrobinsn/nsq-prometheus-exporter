f82e9e60afc41788dce3fa753cba80229356ab74
Michael Robinson <mrobinson@twilio.com>
2020-11-10 16:27:26 -0700

    added support for mTLS authentication

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.12
 
 require (
 	github.com/kr/pretty v0.2.0 // indirect
+	github.com/pkg/errors v0.8.1
 	github.com/prometheus/client_golang v1.5.0
 	github.com/prometheus/common v0.9.1
 	github.com/sirupsen/logrus v1.4.2
--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"caozhipan/nsq-prometheus-exporter/controllers"
 	"crypto/tls"
+	"crypto/x509"
 	"flag"
+	"io/ioutil"
 	"net/http"
 	"time"
 
+	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/common/log"
@@ -15,6 +18,10 @@ import (
 var (
 	nsqLookupdAddress = flag.String("nsq.lookupd.address", "127.0.0.1:4161", "nsqllookupd address list with comma")
 	nsqdScheme        = flag.String("nsq.scheme", "http", "scheme for talking to nsqd over http (http/https)")
+	enableTLS         = flag.Bool("tls", false, "enable mTLS with nsq")
+	caRootCertPath    = flag.String("ca-root-cert-path", "", "path to the root CA certificate")
+	certPath          = flag.String("cert-path", "", "path to the client cert")
+	keyPath           = flag.String("key-path", "", "path to the client cert key")
 )
 
 func main() {
@@ -30,12 +37,35 @@ func main() {
 		}
 	}()
 
-	// support self-signed certs, this is just for stats so not totally insane
-	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	// configure mTLS if enabled
+	if *enableTLS {
+		cfg := tls.Config{}
+		cfg.MinVersion = tls.VersionTLS12
+
+		// Load in the root CA cert
+		rootCAs := x509.NewCertPool()
+		certs, err := ioutil.ReadFile(*caRootCertPath)
+		if err != nil {
+			panic(errors.Errorf("failed to load root ca cert: %v", err))
+		}
+
+		if ok := rootCAs.AppendCertsFromPEM(certs); !ok {
+			panic(errors.New("failed to parse root ca cert"))
+		}
+		cfg.RootCAs = rootCAs
+
+		// Load in the client certificate
+		cert, err := tls.LoadX509KeyPair(*certPath, *keyPath)
+		if err != nil {
+			panic(errors.Errorf("failed to load client certificate: %v", err))
+		}
+		cfg.Certificates = append(cfg.Certificates, cert)
+
+		http.DefaultTransport.(*http.Transport).TLSClientConfig = &cfg
+	}
 
 	prometheus.MustRegister(controllers.Collector)
 
 	http.Handle("/metrics", promhttp.Handler())
 	log.Fatal(http.ListenAndServe(":9527", nil))
-
 }
